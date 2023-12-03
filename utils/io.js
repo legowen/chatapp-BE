147940b0530e696242cf0f0e52390c0588b3942b
OwenSyKim <owen.sykim@gmail.com>
2023-12-02 18:22:26 -0800

    Replace PORT#, add Socket function

--- utils/io.js
+++ utils/io.js
@@ -1,6 +1,10 @@
 module.exports = function (io) {
   // All io Codes are here
-  io.on("Connection", async (socket) => {
-    console.log("Client is connected", socket.id);
+  io.on("connection", async (socket) => {
+    console.log("client is connected", socket.id);
+
+    socket.on("disconnect", () => {
+      console.log("user is disconnected");
+    });
   });
 };
