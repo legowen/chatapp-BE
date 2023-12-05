17ebe3e672d6f54c68f564a470931dd5f3d7eb1a
OwenSyKim <owen.sykim@gmail.com>
2023-12-04 23:47:12 -0800

    Add user Controller

--- utils/io.js
+++ utils/io.js
@@ -1,7 +1,18 @@
+const userController = require("../Controllers/user.controller");
+
 module.exports = function (io) {
   // All io Codes are here
   io.on("connection", async (socket) => {
     console.log("client is connected", socket.id);
+
+    socket.on("login", async (userName, cb) => {
+      try {
+        const user = await userController.saveUser(userName, socket.id);
+        cb({ ok: true, data: user });
+      } catch (error) {
+        cb({ ok: false, error: error.message });
+      }
+    });
 
     socket.on("disconnect", () => {
       console.log("user is disconnected");
