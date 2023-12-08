e27c784233c2f821b9523eb557233f8bdadf1cd2
OwenSyKim <owen.sykim@gmail.com>
2023-12-07 20:03:31 -0800

    Add chatController/Adjust Code

--- Controllers/user.controller.js
+++ Controllers/user.controller.js
@@ -2,7 +2,9 @@ const User = require("../Models/user");
 const userController = {};
 
 userController.saveUser = async (userName, sid) => {
+  //Check registered User
   let user = await User.findOne({ name: userName });
+  // If not, Create New User
   if (!user) {
     user = new User({
       name: userName,
@@ -10,11 +12,17 @@ userController.saveUser = async (userName, sid) => {
       online: true,
     });
   }
-
+  //If Registered User, only change Token value.
   user.token = sid;
   user.online = true;
 
   await user.save();
+  return user;
+};
+
+userController.checkUser = async (sid) => {
+  const user = await User.findOne({ token: sid });
+  if (!user) throw new Error("user not found");
   return user;
 };
 
--- utils/io.js
+++ utils/io.js
@@ -1,4 +1,5 @@
 const userController = require("../Controllers/user.controller");
+const chatController = require("../Controllers/chat.controller");
 
 module.exports = function (io) {
   // All io Codes are here
@@ -6,9 +7,28 @@ module.exports = function (io) {
     console.log("client is connected", socket.id);
 
     socket.on("login", async (userName, cb) => {
+      // Save User Info
       try {
         const user = await userController.saveUser(userName, socket.id);
+        const welcomeMessage = {
+          chat: `${user.name} is joined to this room`,
+          user: { id: null, name: "system" },
+        };
+        io.emit("message", welcomeMessage);
         cb({ ok: true, data: user });
+      } catch (error) {
+        cb({ ok: false, error: error.message });
+      }
+    });
+
+    socket.on("sendMessage", async (message, cb) => {
+      try {
+        //Find User with socket.id
+        const user = await userController.checkUser(socket.id);
+        // SaveMessage function(User)
+        const newMessage = await chatController.saveChat(message, user);
+        io.emit("message", newMessage);
+        cb({ ok: true });
       } catch (error) {
         cb({ ok: false, error: error.message });
       }
